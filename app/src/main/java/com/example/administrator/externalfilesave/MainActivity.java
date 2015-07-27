cac461d6002301b4f62f15f9bc6c206ac9e1d661
lsx <saronic@yeah.net>
2015-07-27 10:08:23 +0800

    可以读文件内容

--- app/src/main/java/com/example/administrator/externalfilesave/MainActivity.java
+++ app/src/main/java/com/example/administrator/externalfilesave/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.administrator.externalfilesave;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -26,14 +30,29 @@ public class MainActivity extends ActionBarActivity {
             FileOutputStream fileOutputStream = null;
 
             try {
+                Log.d("MainActivity", sdPath.toString() + "xxxxxxxxxxxxxxxxx");
                 fileOutputStream = new FileOutputStream(file);
                 fileOutputStream.write("hello".getBytes());
                 fileOutputStream.close();
+                Log.d("MainActivity", sdPath.toString() + "...........");
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            FileInputStream fileInputStream = null;
+            try {
+                fileInputStream = new FileInputStream(file);
+                BufferedReader br = new BufferedReader(new InputStreamReader(fileInputStream));
+                String data = br.readLine();
+                Log.d("lsx", data + "xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
