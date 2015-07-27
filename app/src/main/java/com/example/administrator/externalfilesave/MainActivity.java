84b0c24b418c15fcf313ec8934b352b64ebc5f73
lsx <saronic@yeah.net>
2015-07-27 09:37:12 +0800

    first commit

---
+++ app/src/main/java/com/example/administrator/externalfilesave/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.administrator.externalfilesave;
+
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)) {
+            File sdPath = Environment.getExternalStorageDirectory();
+            File file = new File(sdPath, "data.txt");
+            FileOutputStream fileOutputStream = null;
+
+            try {
+                fileOutputStream = new FileOutputStream(file);
+                fileOutputStream.write("hello".getBytes());
+                fileOutputStream.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
